80e95fd436b3dfde6f67d38681195aeb80353591
Chow Wen Jun <DJwenjun18@gmail.com>
2020-10-14 15:30:42 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 def GUI(game):                              #Opens the GUI to run the game
         for move in game.mainline_moves():
-        board.push(move)
-        time.sleep(2)
-        display.start(board.fen())
+                board.push(move)
+                time.sleep(2)
+                display.start(board.fen())
     
  
