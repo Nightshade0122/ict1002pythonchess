134bfca75d471a98ee4b890fecf815116498b42f
Chow Wen Jun <DJwenjun18@gmail.com>
2020-10-17 23:20:04 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -6,7 +6,18 @@ def GUI(game):                              #Opens the GUI to run the game
         for move in game.mainline_moves():
                 board.push(move)
                 time.sleep(1)
-                display.start(board.fen())
-        display.terminate()
+                boardsvg = chess.svg.board(board=board)
+                f = open("Board.svg", "w")
+                f.write(boardsvg)
+                f.close()
+        window = QWidget()
+        layout = QVBoxLayout()
+        layout.addWidget(QtSvg.QSvgWidget('Board.svg'))
+        layout.addWidget(QPushButton('Top'))
+        layout.addWidget(QPushButton('Bottom'))
+        window.setLayout(layout)
+        window.show()
+        sys.exit(app.exec_())
+
     
  
